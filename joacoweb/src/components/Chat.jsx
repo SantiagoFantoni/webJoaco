a5143413c851a60b22401d689b1e148344d17fbb
Santiago Fantoni <fantonisantiago@gmail.com>
2023-07-26 11:43:40 -0300

    Basic Chat

--- joacoweb/src/components/Chat.jsx
+++ joacoweb/src/components/Chat.jsx
@@ -1,11 +1,31 @@
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3002/");
 import { useEffect, useState } from "react";
 
 function Chat() {
+	useEffect(() => {
+		socket.on("recieved", (data) => {
+			setRecieveMessage(data.message);
+		});
+	}, [socket]);
+	const [messages, setMessages] = useState([]);
+	const [recieveMessage, setRecieveMessage] = useState(null);
+	function sendMessage() {
+		socket.emit("sendingMessages", {
+			message: messages,
+		});
+	}
 	return (
 		<>
-			<div className='chat-box'></div>
-			<input type='text' placeholder='Mensaje...' />
-			<button>Enviar</button>
+			<div className='chat-box'>{recieveMessage}</div>
+			<input
+				type='text'
+				placeholder='Mensaje...'
+				onChange={(e) => {
+					setMessages(e.target.value);
+				}}
+			/>
+			<button onClick={sendMessage}>Enviar</button>
 		</>
 	);
 }
